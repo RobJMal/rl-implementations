8e447998f616eb729ccce6a43495bc98d389dc82
Robert Jomar Malate <robjmal@gmail.com>
2024-05-04 17:00:45 +0200

    Added GPU connection and setting up environment

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ import os
 # Sets the graphics library for MuJoCo, need to set this 
 # before importing dm_control 
 os.environ['MUJOCO_GL'] = 'egl'
+os.environ['MUJOCO_EGL_DEVICE_ID'] = '0'    # Connecting to GPU
 
 # Main imports 
 from dm_control import suite
 import matplotlib.pyplot as plt
+import numpy as np 
 
 if __name__ == '__main__':
-    env = suite.load('cartpole', 'swingup')
+    random_state = np.random.RandomState(42)    # Setting the seed 
+    domain_name, task_name = 'cartpole', 'swingup'
+    env = suite.load(domain_name='cartpole', 
+                     task_name='swingup', 
+                     task_kwargs={'random': random_state})
     pixels = env.physics.render()
 
     plt.imshow(pixels)
