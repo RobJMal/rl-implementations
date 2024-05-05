49e8f95b12a02cd1fc9c7a8a6cfdb20fc1ce5b9a
Robert Jomar Malate <robjmal@gmail.com>
2024-05-05 17:34:32 +0200

    Ran QLearning algorithm

--- gym_main.py
+++ gym_main.py
@@ -4,6 +4,7 @@ import numpy as np
 import time
 
 from sarsa import Sarsa
+from qlearning import QLearning
 
 if __name__ == '__main__':
     env = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=True, render_mode='rgb_array')
@@ -11,10 +12,18 @@ if __name__ == '__main__':
     env.reset(seed=seed_value)
     env.action_space.seed(seed_value)
 
-    sarsa = Sarsa(env=env)
-    sarsa.run()
-    sarsa.plot_results(filename='SARSA_test-0.png')
-    sarsa.record_policy(output_directory="media", video_filename="sarsa_policy-0.mp4")
-    sarsa.plot_q_values_map(filename="sarsa_policy-0.png")
+    # # Running SARSA 
+    # sarsa = Sarsa(env=env)
+    # sarsa.run()
+    # sarsa.plot_results(filename='SARSA_test-0.png')
+    # sarsa.record_policy(output_directory="media", video_filename="sarsa_policy-0.mp4")
+    # sarsa.plot_q_values_map(filename="sarsa_policy-0.png")
+
+    # Running Q-Learning
+    qlarning = QLearning(env=env)
+    qlarning.run()
+    qlarning.plot_results(filename='qlearning_test-0.png')
+    qlarning.record_policy(output_directory="media", video_filename="qlearning_policy-0.mp4")
+    qlarning.plot_q_values_map(filename="qlearning_policy-0.png")
 
     env.close()
