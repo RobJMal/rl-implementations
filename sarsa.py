b6db82bb35bc1f987406ec45af340fa7fb6e0d3c
Robert Jomar Malate <robjmal@gmail.com>
2024-05-05 15:24:37 +0200

    Added plotting feature to visualize results

--- sarsa.py
+++ sarsa.py
@@ -10,38 +10,52 @@ class Sarsa():
         self.Q = np.zeros((self.num_states, self.num_actions))
 
         # Parameters
-        self.epsilon = 0.9
+        self.epsilon = 0.7
         self.discount_factor = 0.95
         self.learning_rate = 0.9
-        self.total_episodes = 10000
+        self.total_episodes = 20000
         self.max_steps = 90    # Number of steps allowed in environment 
 
         self.rewards = []
+        self.test_episodes_range = 100 
+        self.test_episodes = []
+        self.test_frequency = 1000
 
     def run(self):
-        reward = 0
+        print(f"Running SARSA algorithm...")
 
+        reward = 0
         for episode in range(self.total_episodes):
             state_current = self.env.reset()[0]
             action_current = self._choose_action(state_current)
             truncated = False   # Keeps track if episode goes over timelimit 
             terminated = False
 
-            while (not terminated) or (not truncated):
+            while not(terminated or truncated):
                 state_next, reward, terminated, truncated, info = self.env.step(action_current)
                 action_next = self._choose_action(state_next)
-
                 self._update_Q_value(state_current, action_current, state_next, action_next, reward)
 
                 state_current = state_next
                 action_current = action_next
 
-                reward += 1
-            
-            print(f"Completed episode {episode}")
-            print(self.Q)
-        
-    
+            if episode % self.test_frequency == 0:
+                print(f"Evaluating episode {episode}")
+                total_rewards = 0
+
+                for _ in range(self.test_episodes_range):
+                    state = self.env.reset()[0]
+                    terminated, truncated = False, False
+                    while not(terminated or truncated):
+                        action = np.argmax(self.Q[state])
+                        state, reward, terminated, truncated, _ = self.env.step(action)
+                        total_rewards += reward
+                
+                self.rewards.append(total_rewards / self.test_episodes_range)
+                self.test_episodes.append(episode)
+
+        self._plot_results()
+
     def _choose_action(self, state):
         '''
         Chooses an action using epsilon-greedy policy 
@@ -63,3 +77,13 @@ class Sarsa():
         Q_current = self.Q[state, action]
         target = reward + self.discount_factor*self.Q[state_next, action_next]
         self.Q[state, action] = Q_current + self.learning_rate*(target - Q_current)
+
+
+    def _plot_results(self):
+        fig, ax = plt.subplots()
+        ax.plot(self.test_episodes, self.rewards)
+        ax.set_title('Episodes vs average rewards')
+        ax.set_xlabel('Episode')
+        _ = ax.set_ylabel('Average reward')
+
+        plt.show()
