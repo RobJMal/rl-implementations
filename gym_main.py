54825576d29066f500e4351ce93049c7f8101975
Robert Jomar Malate <robjmal@gmail.com>
2024-05-05 14:51:50 +0200

    Imported the SARSA class for use

--- gym_main.py
+++ gym_main.py
@@ -3,22 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import time
 
-# env = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=True)
+from sarsa import Sarsa
 
-env = gym.make("LunarLander-v2", render_mode="human")
-observation, info = env.reset(seed=42)
+env = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=True)
 
-for _ in range(1000):
-    action = env.action_space.sample()
-    observation, reward, terminated, truncated, info = env.step(action)
-
-    print("Action: ", action)
-    print("Observation: ", observation)
-    print("Rewards: ", reward)
-
-    if terminated or truncated:
-        observation, info = env.reset()
-    
-    # time.sleep(0.1)
+sarsa = Sarsa(env=env)
+sarsa.run()
     
 env.close()
