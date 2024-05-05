b22514c63e68cae2ae4bf17cf364fc0f7094afbb
Robert Jomar Malate <robjmal@gmail.com>
2024-05-05 16:09:50 +0200

    Put inside standard python function wrapping for cleanliness

--- gym_main.py
+++ gym_main.py
@@ -5,14 +5,15 @@ import time
 
 from sarsa import Sarsa
 
-env = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=True, render_mode='rgb_array')
-seed_value = 42
-env.reset(seed=seed_value)
-env.action_space.seed(seed_value)
+if __name__ == '__main__':
+    env = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=True, render_mode='rgb_array')
+    seed_value = 42
+    env.reset(seed=seed_value)
+    env.action_space.seed(seed_value)
 
-sarsa = Sarsa(env=env)
-sarsa.run()
-sarsa.plot_results(filename='SARSA_test-0.png')
-sarsa.record_policy(output_directory="media", video_filename="sarsa_policy-0.mp4")
+    sarsa = Sarsa(env=env)
+    sarsa.run()
+    sarsa.plot_results(filename='SARSA_test-0.png')
+    sarsa.record_policy(output_directory="media", video_filename="sarsa_policy-0.mp4")
 
-env.close()
+    env.close()
