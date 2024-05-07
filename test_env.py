dbe870211a37f00c96cc2aa01b16f3c51ef9ce79
Robert Jomar Malate <robjmal@gmail.com>
2024-05-07 02:43:17 +0200

    Changed code to test CartPole-v1 environment

--- test_env.py
+++ test_env.py
@@ -2,15 +2,24 @@ import gymnasium as gym
 import matplotlib.pyplot as plt
 import numpy as np 
 
-env = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=True)
+env = gym.make("CartPole-v1")
 env.action_space.seed(42)
 
-action_space = env.action_space
-num_actions = action_space.n
-print(f"Action space: {action_space} (Total actions: {num_actions})")
+# action_space = env.action_space
+# num_actions = action_space.n
+# print(f"Action space: {action_space} (Total actions: {num_actions})")
+# print("")
 
-observation_space = env.observation_space
-num_observations = observation_space.n
-print(f"Observation space: {observation_space} (Total states: {num_observations})")
+state, _ = env.reset()
+num_observations = len(state)
+observation_space = env.observation_space.shape[0]
+print(observation_space)
 
-# print("Reward range", env.reward_range)
+# num_observations = observation_space.n
+# print(f"Observation space: {observation_space} (Total states: {num_observations})")
+print("")
+
+# state_current = env.reset()
+# action_current = env.action_space.sample()
+# state_next, reward, _, _, _ = env.step(action_current)
+# print("Next state: ", state_next)
